5316822a13d63c0c17cec0d4fdfa09fb258b3817
Aleksey525 <alex_tolchin@mail.ru>
2024-08-28 12:49:30 +0300

    setup DialogFlow

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,21 @@
 import logging
 
 from environs import Env
+from google.cloud import dialogflow
+import telegram
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
-logger = logging.getLogger('Logger')
+def detect_intent_text(project_id, session_id, text, language_code='ru-RUS'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext):
@@ -16,18 +26,24 @@ def start(update: Update, context: CallbackContext):
     )
 
 
-def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+def echo_dialogflow(update: Update, context: CallbackContext):
+    text = update.message.text
+    project_id = context.bot_data['project_id']
+    session_id = update.effective_chat.id
+    f = detect_intent_text(project_id, session_id, text)
+    update.message.reply_text(f)
 
 
 def main():
     env = Env()
     env.read_env()
     bot_token = env.str('TG_BOT_TOKEN')
+    project_id = env.str('PROJECT_ID')
     updater = Updater(bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_dialogflow))
+    dispatcher.bot_data['project_id'] = project_id
     updater.start_polling()
     updater.idle()
 
